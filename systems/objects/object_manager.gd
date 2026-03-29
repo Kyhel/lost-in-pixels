dc8849c5141684eeec124cd54102fc379602e321
Carde Corto <corto.carde@gmail.com>
2026-03-30 01:01:15 +0200

    Optimize get_nearby_world_objects

--- systems/objects/object_manager.gd
+++ systems/objects/object_manager.gd
@@ -120,11 +120,29 @@ func _on_world_object_tree_exiting(world_object: WorldObject) -> void:
 func get_nearby_world_objects(origin: Vector2, radius: float) -> Array[WorldObject]:
 	var results: Array[WorldObject] = []
 	var radius_sq := radius * radius
+	var chunk_px := float(ChunkManager.CHUNK_SIZE * ChunkManager.TILE_SIZE)
 
-	var origin_chunk: Vector2i = ChunkManager.world_pos_to_chunk_coords(origin)
+	var min_x := origin.x - radius
+	var max_x := origin.x + radius
+	var min_y := origin.y - radius
+	var max_y := origin.y + radius
 
-	for cx in range(origin_chunk.x - 1, origin_chunk.x + 2):
-		for cy in range(origin_chunk.y - 1, origin_chunk.y + 2):
+	var min_cx := int(floor(min_x / chunk_px))
+	var max_cx := int(floor(max_x / chunk_px))
+	var min_cy := int(floor(min_y / chunk_px))
+	var max_cy := int(floor(max_y / chunk_px))
+
+	for cx in range(min_cx, max_cx + 1):
+		for cy in range(min_cy, max_cy + 1):
+			var rect_min := Vector2(float(cx), float(cy)) * chunk_px
+			var rect_max := rect_min + Vector2(chunk_px, chunk_px)
+			var nearest_x := clampf(origin.x, rect_min.x, rect_max.x)
+			var nearest_y := clampf(origin.y, rect_min.y, rect_max.y)
+			var ddx := origin.x - nearest_x
+			var ddy := origin.y - nearest_y
+			if ddx * ddx + ddy * ddy > radius_sq:
+				continue
+
 			var key: Vector2i = Vector2i(cx, cy)
 			var chunk: Chunk = ChunkManager.get_loaded_chunk(key)
 			if chunk == null:
