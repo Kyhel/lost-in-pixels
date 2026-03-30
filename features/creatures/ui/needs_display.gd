802c238fc325ab49bee45a9ac222d5f58833ac6b
Carde Corto <corto.carde@gmail.com>
2026-03-31 01:59:39 +0200

    Connect directly to need instance

--- features/creatures/ui/needs_display.gd
+++ features/creatures/ui/needs_display.gd
@@ -4,8 +4,10 @@ extends Control
 func set_watch(creature: Creature) -> void:
 	if creature.needs_component == null:
 		return
-	var hunger_cb := func(id: StringName, value: float) -> void:
-		if id == NeedIds.HUNGER:
-			$HungerBar.value = value
-	creature.needs_component.need_value_changed.connect(hunger_cb)
+	var hunger_cb := func(_id: StringName, value: float) -> void:
+		$HungerBar.value = value
+	var hunger_need := creature.needs_component.get_instance(NeedIds.HUNGER)
+	if hunger_need != null:
+		hunger_need.value_changed.connect(hunger_cb)
+	
 	$HungerBar.value = creature.needs_component.get_need_value(NeedIds.HUNGER)
