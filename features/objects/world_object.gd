222fadac2c7a0af514bc6f8405c9de1afc570157
Carde Corto <corto.carde@gmail.com>
2026-04-06 20:50:57 +0200

    Refacto

--- features/objects/world_object.gd
+++ features/objects/world_object.gd
@@ -17,25 +17,15 @@ func add_behavior_instance(instance: WorldObjectBehaviorInstance) -> void:
 	behavior_instances.append(instance)
 
 func _ready() -> void:
+
 	if object_data == null:
 		return
-	if object_data.texture:
-		var sprite := get_node_or_null("Sprite2D") as Sprite2D
-		if sprite != null:
-			sprite.texture = object_data.texture
-			var tex_size: Vector2 = sprite.texture.get_size()
-			var target_dim: float = object_data.hitbox_radius * 2.0
-			sprite.scale = Vector2.ONE * (target_dim / maxf(tex_size.x, tex_size.y))
-	var cs: CollisionShape2D = get_node_or_null("CollisionShape2D") as CollisionShape2D
-	if cs != null:
-		var base_shape: Shape2D = cs.shape
-		if base_shape is CircleShape2D:
-			var base_r: float = (base_shape as CircleShape2D).radius
-			if base_r > 0.0:
-				cs.scale = Vector2.ONE * (object_data.hitbox_radius / base_r)
+	
+	z_index = Constants.Z_INDEX_OBJECTS
+
+	_apply_scale()
 	_apply_features()
 	_apply_behaviors()
-	z_index = Constants.Z_INDEX_OBJECTS
 
 func be_eaten(by: Creature) -> void:
 	ObjectInteractionSystem.do_eat(by, self)
@@ -58,3 +48,19 @@ func _apply_features() -> void:
 		if feature == null:
 			continue
 		feature.apply(self)
+
+func _apply_scale() -> void:
+	if object_data.texture:
+		var sprite := get_node_or_null("Sprite2D") as Sprite2D
+		if sprite != null:
+			sprite.texture = object_data.texture
+			var tex_size: Vector2 = sprite.texture.get_size()
+			var target_dim: float = object_data.hitbox_radius * 2.0
+			sprite.scale = Vector2.ONE * (target_dim / maxf(tex_size.x, tex_size.y))
+	var cs: CollisionShape2D = get_node_or_null("CollisionShape2D") as CollisionShape2D
+	if cs != null:
+		var base_shape: Shape2D = cs.shape
+		if base_shape is CircleShape2D:
+			var base_r: float = (base_shape as CircleShape2D).radius
+			if base_r > 0.0:
+				cs.scale = Vector2.ONE * (object_data.hitbox_radius / base_r)
