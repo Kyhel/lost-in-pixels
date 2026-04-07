99704072043c4750e3ac9cff747dedc609e2d541
Carde Corto <corto.carde@gmail.com>
2026-04-07 20:34:03 +0200

    Add night flower harvesting

--- features/objects/behaviors/pickable_behavior.gd
+++ features/objects/behaviors/pickable_behavior.gd
@@ -2,14 +2,18 @@ class_name PickableBehavior
 extends WorldObjectBehavior
 
 @export var item_id: StringName
+@export var requires_night: bool = false
 
 
 static func get_pickup_item(od: ObjectData) -> ItemData:
 	for b in od.behaviors:
 		if b is PickableBehavior:
 			var pickable := b as PickableBehavior
-			if not pickable.item_id.is_empty():
-				return ItemDatabase.get_item_data(pickable.item_id)
+			if pickable.item_id.is_empty():
+				continue
+			if pickable.requires_night and not DayNightCycle.is_night_phase():
+				continue
+			return ItemDatabase.get_item_data(pickable.item_id)
 	return null
 
 
@@ -18,9 +22,4 @@ static func has_behavior(wo: WorldObject) -> bool:
 	if wo == null or wo.object_data == null:
 		return false
 
-	for b in wo.object_data.behaviors:
-		if b is PickableBehavior:
-			var pickable := b as PickableBehavior
-			if not pickable.item_id.is_empty():
-				return true
-	return false
+	return get_pickup_item(wo.object_data) != null
